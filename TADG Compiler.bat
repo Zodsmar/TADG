670d9f49d5c962570085a332ebf0acfb44ddf538
Unknown <zodsmar@gmail.com>
2018-02-07 18:21:27 -0500

    Compiler Fix
    Not using absolute path anymore :P

--- TADG Compiler.bat
+++ TADG Compiler.bat
@@ -1,4 +1,5 @@
 echo off
+set path=%~dp0
 title TADG Compiler
-tweego -s Main -o D:\Users\Alexander\Documents\Tweego\TADG\output\TADG.html D:\Users\Alexander\Documents\Tweego\TADG\src
-start "" "D:\Users\Alexander\Documents\Tweego\TADG\output\TADG.html"
+tweego -s Main -o %path%\output\TADG.html %path%\src
+start "" "%path%\output\TADG.html"
